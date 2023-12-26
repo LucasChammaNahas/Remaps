282f06cca96b5ff8af548ec2f1d0a51999a71ad7
Lucas Chamma Nahas <lucasnahas@gmail.com>
2023-12-26 00:27:04 +0000

    save

---
+++ Vim2/Utils/getTargetedMotions.mjs
@@ -0,0 +1,72 @@
+export function getTargetedMotions() {
+  const remaps = [];
+
+  for (const key of keyToSymbolMap) {
+    remaps.push(getObject(key[0], key[1], 'n', 'Forwards'));
+    remaps.push(getObject(key[0], key[1], 'v', 'Forwards'));
+    remaps.push(getObject(key[0], key[1], 'n', 'Backwards'));
+    remaps.push(getObject(key[0], key[1], 'v', 'Backwards'));
+  }
+
+  return remaps;
+}
+
+function getObject(key0, key1, mode, type) {
+  let afterSuffix = '';
+  let leader = '';
+
+  if (mode === 'n' && type === 'Forwards') {
+    afterSuffix = 'f';
+  }
+  if (mode === 'v' && type === 'Forwards') {
+    afterSuffix = 't';
+  }
+  if (mode === 'n' && type === 'Backwards') {
+    afterSuffix = 'F';
+    leader = '<leader>';
+  }
+  if (mode === 'v' && type === 'Backwards') {
+    afterSuffix = 'T';
+    leader = '<leader>';
+  }
+
+  return {
+    comment: `Target ${type} Special Symbol`,
+    modes: mode,
+    before: [leader + 'n' + key0],
+    after: afterSuffix + key1,
+  };
+}
+
+const keyToSymbolMap = [
+  'q#',
+  'w|',
+  'e[',
+  'r]',
+  't~',
+  'a@',
+  's(',
+  'd)',
+  'f;',
+  'g:',
+  'z/',
+  'x*',
+  'c+',
+  'v%',
+  'b$',
+  'y&',
+  'u<',
+  'i=',
+  'o>',
+  'p!',
+  'h`',
+  'j{',
+  'k}',
+  "l'",
+  '\'"',
+  'n-',
+  'm_',
+  ',,',
+  '..',
+  '/?',
+];
--- Vim2/config.mjs
+++ Vim2/config.mjs
@@ -1,21 +1,169 @@
-import * as f from './functions.mjs';
+import { getTargetedMotions } from './utils/getTargetedMotions.mjs';
 
 export const config = {
+  modes: {
+    switching: [
+      {
+        comment: 'Visual Line',
+        before: 'cv',
+        after: 'V',
+      },
+      {
+        comment: 'Visual Block',
+        before: 'cc',
+        after: '<C-v>',
+      },
+    ],
+  },
+
   movements: {
     basic: [
-      // ...f.getMovements({ comment: 'Move Cursor' }),
-      ...f.getMovements({
-        comment: 'Move Cursor Strong',
-        before: '<leader>',
-        afterY: '5',
-        afterX: '12',
-      }),
-      // ...f.getMovements({
-      //   comment: 'Move Cursor Stronger',
-      //   after: '10',
-      //   direction: 'y',
-      //   modifiers: ['shift'],
-      // }),
+      {
+        comment: 'Move Cursor Up',
+        before: ['i'],
+        after: 'k',
+      },
+      {
+        comment: 'Move Cursor Down',
+        before: ['k'],
+        after: 'j',
+      },
+      {
+        comment: 'Move Cursor Left',
+        before: ['j'],
+        after: 'h',
+      },
+      {
+        comment: 'Move Cursor Right',
+        before: ['l'],
+        after: 'l',
+      },
+      {
+        comment: 'Move Cursor Strong Up',
+        before: ['<leader>I'],
+        after: '5k',
+      },
+      {
+        comment: 'Move Cursor Strong Down',
+        before: ['<leader>K'],
+        after: '5j',
+      },
+      {
+        comment: 'Move Cursor Strong Left',
+        before: ['<leader>J'],
+        after: '12h',
+      },
+      {
+        comment: 'Move Cursor Strong Right',
+        before: ['<leader>L'],
+        after: '12l',
+      },
+      {
+        comment: 'Move Cursor Stronger Up',
+        before: ['I'],
+        after: '10k',
+      },
+      {
+        comment: 'Move Cursor Stronger Down',
+        before: ['K'],
+        after: '10j',
+      },
+      {
+        comment: 'Move Cursor Stronger Left',
+        before: ['J'],
+        after: '10h',
+      },
+      {
+        comment: 'Move Cursor Stronger Right',
+        before: ['L'],
+        after: '10l',
+      },
     ],
+
+    word: [
+      {
+        comment: 'Go to Start of Word',
+        before: ['w;'],
+        after: 'w',
+      },
+      {
+        comment: 'Go to Start of WORD',
+        before: ['ww;'],
+        after: 'W',
+      },
+      {
+        comment: 'Go to End of Word',
+        before: ['wl'],
+        after: 'e',
+      },
+      {
+        comment: 'Go to Start of WORD',
+        before: ['wwl'],
+        after: 'E',
+      },
+      {
+        comment: 'Go Back to Start of Word',
+        before: ['wj'],
+        after: 'b',
+      },
+      {
+        comment: 'Go Back to Start of WORD',
+        before: ['wwj'],
+        after: 'B',
+      },
+      {
+        comment: 'Go Back to End of Word',
+        before: ['wh'],
+        after: 'ge',
+      },
+      {
+        comment: 'Go Back to Start of WORD',
+        before: ['wwh'],
+        after: 'gE',
+      },
+    ],
+
+    line: [
+      {
+        comment: 'Go to Start of Line',
+        before: ['h'],
+        after: '^',
+      },
+      {
+        comment: 'Go to End of Line',
+        before: [';'],
+        after: 'g_',
+      },
+    ],
+
+    ample: [
+      {
+        comment: 'Go to Top of Screen',
+        before: ['gi'],
+        after: 'H',
+      },
+      {
+        comment: 'Go to Middle of Screen',
+        before: ['gj', 'gl'],
+        after: 'M',
+      },
+      {
+        comment: 'Go to Bottom of Screen',
+        before: ['gk'],
+        after: 'L',
+      },
+      {
+        comment: 'Go to Top of Document',
+        before: ['ggi'],
+        after: 'gg',
+      },
+      {
+        comment: 'Go to Bottom of Document',
+        before: ['ggk'],
+        after: 'G',
+      },
+    ],
+
+    targeted: getTargetedMotions(),
   },
 };
