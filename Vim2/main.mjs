8cb2811fcd5364a1d1a5f7eaaa7c4363ac076db3
Lucas Chamma Nahas <lucasnahas@gmail.com>
2023-12-30 00:42:21 +0000

    finished vim

--- Vim2/main.mjs
+++ Vim2/main.mjs
@@ -7,18 +7,37 @@ import {
   generateCorrectVimStructure,
 } from './Utils/generateCorrectStructure.mjs';
 
+const modes = {
+  normal: 'vim.normalModeKeyBindingsNonRecursive',
+  visual: 'vim.visualModeKeyBindingsNonRecursive',
+  pending: 'vim.operatorPendingModeKeyBindingsNonRecursive',
+};
+
+const finalJson = {
+  [modes.normal]: [],
+  [modes.visual]: [],
+  [modes.pending]: [],
+};
+
 const commandList = generateListWithAllCommands(config);
 
 for (const command of commandList) {
   const commandTypes = getCommandTypes(command);
   let updatedCommand;
-  
+
   if (commandTypes.includes('code')) {
     updatedCommand = generateCorrectCodeStructure(command);
   } else {
     updatedCommand = generateCorrectVimStructure(command);
+    for (const type of commandTypes) {
+      finalJson[modes[type]].push(updatedCommand);
+    }
   }
 }
 
-// const jsonData = JSON.stringify(config, null, 2);
-// fs.writeFileSync('output.json', jsonData);
+console.log('normal',finalJson[modes.normal].length);
+console.log('visual',finalJson[modes.visual].length);
+console.log('pending',finalJson[modes.pending].length);
+
+const jsonData = JSON.stringify(finalJson, null, 2);
+fs.writeFileSync('output.json', jsonData);
